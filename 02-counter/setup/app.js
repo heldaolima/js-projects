6cd29a681cc56aab4785536a8819060e2ac98549
Hélder Silva Ferreira Lima <74564010+heldaolima@users.noreply.github.com>
2022-03-24 12:01:37 -0300

    primeiros 3 projetos

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,30 @@
+let count = 0;
+let value = document.getElementById('value');
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('reset')) {
+            count = 0;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+        else if (count > 0) {
+            value.style.color = 'green';
+        }
+        else {
+            value.style.color = '#222'
+        }
+        value.textContent = count;
+    });
+});
